53391e556d8dea3a515f3ee0b58ba0b4edf8b3cb
Draxnol <kaczurr@gmail.com>
2017-12-30 00:13:25 -0500

    Added directory creation for output files

--- multiScript/fileReader.py
+++ multiScript/fileReader.py
@@ -1,5 +1,6 @@
 from pydub import AudioSegment
 import csv
+import os
 class FileReader():
     def processFile(self,filename):
         with open(filename, newline='') as ofile:
@@ -14,14 +15,15 @@ class FileReader():
                         fileName = data
                     elif index > 0:
                         if index%2 == 0:
-                            print('timestamp is: ' + data)
                             #append timestamp to array
                             intNum = int(data)
                             storyTimes.append(intNum * 60000)
                         else:
                             #append storyNames
-                            print('story name is: ' + data)
                             storyNames.append(data)
+                print('--------------')
+                print('starting work  on '+fileName)
+                print('--------------')
                 status = self.processAudio(fileName,storyTimes,storyNames)
                 print('--------------')
                 print(status)
@@ -33,10 +35,17 @@ class FileReader():
         #load file
         startSlice = 0
         file = AudioSegment.from_file(fileName)
+        ####
+        x = str(fileName)
+        splitFileName = x.split('.')
+        dirName = './files/{}'.format(splitFileName[0])
+        os.mkdir(dirName)
+          
         for index, name in enumerate(storyNames):
-            print('------'+str(index) + ' ' + name)
-            fileSplitName = '{}.mp3'.format(name)
+            print(str(index) + ' ' + name + ' done')
+            #format pathname where files will go
+            fileSplitName = '{}/{}.mp3'.format(dirName,name)
             toExport = file[startSlice:int(storyTimes[index])]
             startSlice = storyTimes[index]
             toExport.export(fileSplitName, format='mp3')
-        return 'done'
+        return 'done ' + fileName
